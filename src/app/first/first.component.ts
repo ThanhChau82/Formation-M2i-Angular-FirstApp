44f535ec4e02811a1151f331ee2d53d401a1e42d
ThanhChau82 <thanh.dalencon@gmail.com>
2021-06-09 15:26:07 +0200

    Démo Binding d'évènement

--- src/app/first/first.component.ts
+++ src/app/first/first.component.ts
@@ -1,14 +1,28 @@
-import { Component } from "@angular/core";
+import { Component, OnInit } from "@angular/core";
 
 @Component({
     selector : 'app-first',
     templateUrl : './first.component.html'
 })
-export class FirstComponent{
+export class FirstComponent implements OnInit{
+  ngOnInit(): void {
+    this.url = this.url1;
+  }
   titre = "Exemple d'interpolation";
-  url = "https://images.app.goo.gl/aKMjRY2SQqmYZviM9";
+  url = "";
+  url1 = "https://w7.pngwing.com/pngs/224/196/png-transparent-web-development-angularjs-javascript-vue-js-world-wide-web-thumbnail.png";
+  url2 = "https://w7.pngwing.com/pngs/421/958/png-transparent-angularjs-javascript-web-application-others-angle-logo-sign-thumbnail.png";
 
   afficherBonjour() : string {
     return "Bonjour";
+  }
+
+  changerLogoAngular() {
+    if (this.url = this.url1) {
+      this.url = this.url2;
+    } else {
+      this.url = this.url1;
+    }
+
   }
 }
