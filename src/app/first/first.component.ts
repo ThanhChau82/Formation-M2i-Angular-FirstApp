3d128a6490054af29b5891a8af1c21cc3995dc4f
ThanhChau82 <thanh.dalencon@gmail.com>
2021-06-09 15:14:17 +0200

    Démo binding de propriété

--- src/app/first/first.component.ts
+++ src/app/first/first.component.ts
@@ -6,6 +6,7 @@ import { Component } from "@angular/core";
 })
 export class FirstComponent{
   titre = "Exemple d'interpolation";
+  url = "https://images.app.goo.gl/aKMjRY2SQqmYZviM9";
 
   afficherBonjour() : string {
     return "Bonjour";
