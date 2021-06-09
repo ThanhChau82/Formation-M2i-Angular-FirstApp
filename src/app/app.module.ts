24b7d2c27e30b899d79fc27d34605b316dfe3d8b
ThanhChau82 <thanh.dalencon@gmail.com>
2021-06-09 15:44:11 +0200

    Two-way data binding

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,4 +1,5 @@
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 import { BrowserModule } from '@angular/platform-browser'; // Translateur pour les navigateurs
 
 import { AppComponent } from './app.component';
@@ -12,7 +13,8 @@ import { SecondComponent } from './second/second.component';
     SecondComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/first/first.component.ts
+++ src/app/first/first.component.ts
@@ -8,7 +8,7 @@ export class FirstComponent implements OnInit{
   ngOnInit(): void {
     this.url = this.url1;
   }
-  titre = "Exemple d'interpolation";
+  titre = "Ex d'interpolation";
   url = "";
   url1 = "https://w7.pngwing.com/pngs/224/196/png-transparent-web-development-angularjs-javascript-vue-js-world-wide-web-thumbnail.png";
   url2 = "https://w7.pngwing.com/pngs/421/958/png-transparent-angularjs-javascript-web-application-others-angle-logo-sign-thumbnail.png";
