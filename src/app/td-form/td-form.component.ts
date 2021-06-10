9b2b60a235d250a7680f666c92042669bbad4633
ThanhChau82 <thanh.dalencon@gmail.com>
2021-06-10 12:26:28 +0200

    td-formComponent : ajouter un pays dans la liste des pays

--- src/app/td-form/td-form.component.ts
+++ src/app/td-form/td-form.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { NgForm } from '@angular/forms';
+import { Pays } from '../models/pays';
 
 @Component({
   selector: 'app-td-form',
@@ -7,14 +8,20 @@ import { NgForm } from '@angular/forms';
   styleUrls: ['./td-form.component.css']
 })
 export class TdFormComponent implements OnInit {
+  p : Pays;
+  pays : Pays[];
 
-  constructor() { }
+  constructor() {
+    this.p = <Pays>{};
+    this.pays = [];
+  }
 
   ngOnInit(): void {
   }
 
   validerFormulaire(f : NgForm) {
-    console.log(f);
+    //console.log(f);
+    this.p = new Pays(f.value.id, f.value.nom, f.value.langue, f.value.drapeau, f.value.capitale);
+    this.pays.push(this.p);
   }
-
 }
